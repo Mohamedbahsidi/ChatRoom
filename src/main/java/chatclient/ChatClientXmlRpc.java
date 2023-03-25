f3251a83ce0867b8d1cd4e40c770479d9eef5093
Mohamed <mmohamedoulbah@gmail.com>
2023-03-25 09:27:15 +0100

    mon 2 eme commit

--- src/main/java/chatclient/ChatClientXmlRpc.java
+++ src/main/java/chatclient/ChatClientXmlRpc.java
@@ -2,9 +2,13 @@ package chatclient;
 import java.util.*;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URL;
+import java.rmi.RemoteException;
 
+import org.apache.*;
 import org.apache.xmlrpc.XmlRpcException;
 import org.apache.xmlrpc.client.XmlRpcClient;
+import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
 
 import chat.ChatUser;
 
@@ -16,19 +20,21 @@ public class ChatClientXmlRpc implements ChatUser {
     public ChatClientXmlRpc(String nickname, String serverUrl) throws MalformedURLException {
         this.nickname = nickname;
         this.serverUrl=serverUrl;
-        this.xmlRpcClient = new XmlRpcClient(this.ser);
+     
+        this.xmlRpcClient=new XmlRpcClient();
+       
     }
 
     public void subscribe() throws XmlRpcException, IOException {
-        xmlRpcClient.execute(" ChatRoom.join", new Object[]{nickname, this});
+        xmlRpcClient.execute(" ChatRoom.subscribe", new Object[]{nickname, this});
     }
 
     public void unsubscribe() throws XmlRpcException, IOException {
-        xmlRpcClient.execute("ChatServer.leave", new Object[]{nickname});
+        xmlRpcClient.execute("ChatServer.unsubscribe", new Object[]{nickname});
     }
 
     public void postMessage(String message) throws XmlRpcException, IOException {
-        xmlRpcClient.execute("ChatServer.sendMessage", new Object[]{nickname, message});
+        xmlRpcClient.execute("ChatServer.postMessage", new Object[]{nickname, message});
     }
 
     public void displayMessage(String sender, String message) {
@@ -40,6 +46,15 @@ public class ChatClientXmlRpc implements ChatUser {
         String nickname = "Alice";
 
         ChatClientXmlRpc client = new ChatClientXmlRpc(nickname, serverUrl);
+      //  client.setEndpoint("http://www.example.com/xmlrpc");
+     // create a configuration instance with the requested URL
+        XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
+config.setServerURL(new URL("http://localhost/RPC2"));
+
+        // create the client and configure it with instantiated configuration
+        XmlRpcClient server = new XmlRpcClient();
+        server.setConfig(config);
+        
         client.subscribe();
 
         Scanner scanner = new Scanner(System.in);
@@ -52,3 +67,9 @@ public class ChatClientXmlRpc implements ChatUser {
             client.postMessage(message);
         }
    
+
+
+	public void displayMessage(String message) throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}}
